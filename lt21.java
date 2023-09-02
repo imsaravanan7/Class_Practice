05d9e48c97f55518e41d40d72f898fe10535694f
imsaravanan <imsaravanan5203@gmail.com>
2023-09-02 19:19:05 +0530

    my commit to det loop

--- lt21.java
+++ lt21.java
@@ -5,18 +5,7 @@ public class lt21{
         int a;
         System.out.println("enter a value");
         a=scan.nextInt();
-        for(int i=1;i<=a;i++){
-            for(int j=i;j<=a;j++){
-                System.out.print(" ");
-            }
-            for(int k=i;k>=1;k--){
-                System.out.print(k);
-            }
-            for(int l=2;l<=i;l++){
-                System.out.print(l);
-            
-            }
-            System.out.println();
-        }
+     
+    
     }
 }
